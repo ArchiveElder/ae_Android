f0c0d20a4f4c064b44d5181fe11f1416a458a7ed
rosesua318 <rosesua@duksung.ac.kr>
2022-03-16 20:21:16 +0900

    mmse_k layout

---
+++ app/src/main/java/com/example/ae_android/ui/mmse/MMSEFragment.java
@@ -0,0 +1,38 @@
+package com.example.ae_android.ui.mmse;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ae_android.R;
+
+public class MMSEFragment extends Fragment {
+
+    private MMSEViewModel mViewModel;
+
+    public static MMSEFragment newInstance() {
+        return new MMSEFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.mmse_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MMSEViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
