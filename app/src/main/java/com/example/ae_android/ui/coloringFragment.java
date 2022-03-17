b2348b7b113c8a87e12343716487b926d7b38d7b
안소정 <80646857+ahneve@users.noreply.github.com>
2022-03-17 15:02:58 +0900

    컬러링 프래그먼트 팝업

--- app/src/main/java/com/example/ae_android/ui/coloringFragment.java
+++ app/src/main/java/com/example/ae_android/ui/coloringFragment.java
@@ -1,5 +1,6 @@
 package com.example.ae_android.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.ae_android.R;
 
@@ -20,6 +22,24 @@ public class coloringFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_coloring, container, false);
 
+        ImageButton imageUpload_button = view.findViewById(R.id.imageUpload_button);
+
+        //사진등록 버튼 클릭시 이벤기
+        imageUpload_button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //팝업 띄우기
+                final View coloringPopup = getLayoutInflater().inflate(R.layout.fragment_coloring_popup, null);
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setView(coloringPopup);
+
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+
+                }
+        }) ;
 
         return view;
 
