73d0611615bed844ae6eda832b1221a311f4595e
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-03-17 21:22:13 +0900

    컬러링 테스트(one color)

---
+++ app/src/main/java/com/example/ae_android/ui/coloring/ColorOnPicFragment.java
@@ -0,0 +1,34 @@
+package com.example.ae_android.ui.coloring;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ae_android.R;
+
+
+public class ColorOnPicFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_color_on_pic, container, false);
+
+        return new ColorOnPicView(getActivity());
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
