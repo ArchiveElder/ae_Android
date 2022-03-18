dff1b97f25008b0ea491aa5671aef7ec5973a05f
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-03-18 17:43:44 +0900

    컬러링 페이지 뒤 도안 추가

--- app/src/main/java/com/example/ae_android/ui/coloring/ColorOnPicFragment.java
+++ app/src/main/java/com/example/ae_android/ui/coloring/ColorOnPicFragment.java
@@ -1,7 +1,10 @@
 package com.example.ae_android.ui.coloring;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,15 +17,18 @@ import android.view.ViewGroup;
 import com.example.ae_android.R;
 
 
+
+
 public class ColorOnPicFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_color_on_pic, container, false);
+         View view = inflater.inflate(R.layout.fragment_color_on_pic, container, false);
 
-        return new ColorOnPicView(getActivity());
+
+        return view;
     }
 
     @Override
@@ -31,4 +37,7 @@ public class ColorOnPicFragment extends Fragment {
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/ae_android/ui/coloring/ColorOnPicView.java
+++ app/src/main/java/com/example/ae_android/ui/coloring/ColorOnPicView.java
@@ -5,8 +5,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+
 
 public class ColorOnPicView  extends View {
     private Paint paint = new Paint();
@@ -14,10 +16,47 @@ public class ColorOnPicView  extends View {
     private Path path = new Path();
     private int x,y;
 
+
+
     public ColorOnPicView(Context context) {
         super(context);
     }
+    public ColorOnPicView(Context context, AttributeSet attrs){
+        super(context,attrs);
 
+    }
+    public ColorOnPicView(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+/*
+    //Canvas 크기 설정
+    int height, width =0;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //setMeasuredDimension(500,500);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+
+        if(heightMode == MeasureSpec.AT_MOST || heightMode == MeasureSpec.UNSPECIFIED){
+            height = 200;
+        }else if(heightMode == MeasureSpec.EXACTLY){
+            height = heightSize;
+        }
+
+        if(widthMode == MeasureSpec.AT_MOST || widthMode == MeasureSpec.UNSPECIFIED){
+            width = 200;
+        }else if(widthMode == MeasureSpec.EXACTLY){
+            width = widthSize;
+        }
+
+        setMeasuredDimension(width,height);
+    }
+*/
     @Override
     protected void onDraw(Canvas canvas) {
         paint.setColor(Color.GREEN);
@@ -26,6 +65,8 @@ public class ColorOnPicView  extends View {
         paint.setAlpha(30);     //투명도
         canvas.drawPath(path,paint);
     }
+
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
